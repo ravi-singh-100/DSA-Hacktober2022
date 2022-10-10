41e03de6334871ba33c85bddf533f6476a2d01a0
Chandan <109679579+I-am-CHANDAN@users.noreply.github.com>
2022-10-10 11:00:27 +0530

    cpp program

---
+++ Cpp program/find_Even_Odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+    {
+        if (n % 2 == 0)
+        {
+            cout << "Even" << endl;
+        }
+        else
+        {
+            cout << "Odd" << endl;
+        }
+    }
+    return 0;
+}
